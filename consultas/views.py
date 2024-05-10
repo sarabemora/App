3baebff70c1d89e2f54c53899294e00df062e4e4
Sara Benavides <s.benavidesm@uniandes.edu.co>
2024-05-10 12:01:32 -0500

    verificacion de rol en consultas

--- consultas/views.py
+++ consultas/views.py
@@ -5,15 +5,21 @@ from .logic import consultas_logic as cl
 import json
 from .forms import ConsultasForm
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from app.auth0backend import getRole
 
+@login_required
 @csrf_exempt
 def consultas_view(request):
-    if request.method == 'GET':
-        form = ConsultasForm()
-        return render(request, 'Consulta/consultas.html', {'form': form})
+    role = getRole(request)
+    if role == "Admin":
+        if request.method == 'GET':
+            form = ConsultasForm()
+            return render(request, 'Consulta/consultas.html', {'form': form})
     
-    if request.method == 'POST':
-        cedula = request.POST.get('id')
-        consulta = cl.get_consulta_by_cedula(cedula)
-        return render(request, 'Consulta/consultas.html', {'consulta': consulta})
+        if request.method == 'POST':
+            cedula = request.POST.get('id')
+            consulta = cl.get_consulta_by_cedula(cedula)
+            return render(request, 'Consulta/consultas.html', {'consulta': consulta})
  
+
