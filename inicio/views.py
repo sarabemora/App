9190776999e7df184f6424a2c66af4c781332de5
Sara Benavides <s.benavidesm@uniandes.edu.co>
2024-05-10 12:04:25 -0500

    verificación usuario inicio

--- inicio/views.py
+++ inicio/views.py
@@ -1,18 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 from .logic import inicio_logic as il
+from app.auth0backend import getRole
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def login_view(request):
-    if request.method == 'POST':
-        id = request.POST.get('id')
-        passwords = request.POST.get('password')
-        inicio = il.get_login(id, passwords)
-        return render(request, 'Inicio/inicios.html', {'inicio': inicio})
+    role = getRole(request)
+    if role == 'Admin':
+        if request.method == 'POST':
+            id = request.POST.get('id')
+            passwords = request.POST.get('password')
+            inicio = il.get_login(id, passwords)
+            return render(request, 'Inicio/inicios.html', {'inicio': inicio})
 
-    if request.method == 'GET':
-        form = LoginForm()
+        if request.method == 'GET':
+            form = LoginForm()
+            return render(request, 'Inicio/inicios.html', {'form': form})
+        else:
+            form = LoginForm()
         return render(request, 'Inicio/inicios.html', {'form': form})
     else:
-        form = LoginForm()
-    return render(request, 'Inicio/inicios.html', {'form': form})
+        return HttpResponse("No tienes permisos para ver esta página")
